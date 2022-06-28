9d5d1b8db163513f14de8cf4cdf6110e223a2595
Larissa Miranda <laalnuquerque@gmail.com>
2022-06-28 12:09:00 -0300

    criacao da service professor

--- src/main/java/com/mentoriajava/academia/services/ProfessorService.java
+++ src/main/java/com/mentoriajava/academia/services/ProfessorService.java
@@ -1,5 +1,14 @@
 package com.mentoriajava.academia.services;
 
-public class ProfessorService {
+import com.mentoriajava.academia.model.dto.AlunoDto;
+import com.mentoriajava.academia.model.dto.ProfessorDto;
 
+public class ProfessorService {
+    public void cadastroProfessor(ProfessorDto professorDto){
+        AlunoDto professor = new AlunoDto();
+        professor.setId(professorDto.getId());
+        professor.setNome(professorDto.getNome());
+        professor.setCpf(professorDto.getCpf());
+
+    }
 }
